8bf625add7eeff853376371c095607fbf5fa6186
AaronCoplan <afcoplan@gmail.com>
2026-03-17 22:30:21 -0400

    Updates to prioritize emacs mode over vim

--- configs/doom/config.el
+++ configs/doom/config.el
@@ -1,5 +1,9 @@
 ;;; config.el -*- lexical-binding: t; -*-
 
+;; Default to emacs-style keybindings in all buffers
+(setq evil-default-state 'emacs)
+(add-hook 'after-change-major-mode-hook 'evil-emacs-state)
+
 ;; Indentation settings
 (setq-default c-basic-offset 8)
 (add-hook 'java-mode-hook (lambda ()
