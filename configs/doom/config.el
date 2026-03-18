f5e2638fe0eac25a8e0b64cd532f2c108f93517b
AaronCoplan <afcoplan@gmail.com>
2026-03-17 22:37:18 -0400

    Remove emacs prompt for "are you sure" when there are no unsaved changes on exit.

--- configs/doom/config.el
+++ configs/doom/config.el
@@ -4,6 +4,11 @@
 (setq evil-default-state 'emacs)
 (add-hook 'after-change-major-mode-hook 'evil-emacs-state)
 
+;; Only confirm exit if there are unsaved changes
+(setq confirm-kill-emacs
+      (lambda (prompt)
+        (if (buffer-modified-p) (yes-or-no-p prompt) t)))
+
 ;; Indentation settings
 (setq-default c-basic-offset 8)
 (add-hook 'java-mode-hook (lambda ()
